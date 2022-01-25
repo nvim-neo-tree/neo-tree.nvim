8e22bbba7f645b6996f67819f7fea66f5e985b52
Joakim Gottzén <joakim.gottzen@gmail.com>
2022-01-25 14:39:58 +0100

    Handle empty reponses from git

--- lua/neo-tree/git/init.lua
+++ lua/neo-tree/git/init.lua
@@ -8,7 +8,8 @@ local M = {}
 local function execute_command(cmd)
   local result = vim.fn.systemlist(cmd)
 
-  if vim.v.shell_error ~= 0 or vim.startswith(result[1], "fatal:") then
+  -- An empty result is ok
+  if vim.v.shell_error ~= 0 or (#result > 0 and vim.startswith(result[1], "fatal:")) then
     return false, {}
   else
     return true, result
