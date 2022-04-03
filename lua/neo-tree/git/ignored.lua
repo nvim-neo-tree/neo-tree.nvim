9f049904000ac6c6cc7bb4845bbff055c8dbb91b
Chris Seickel <cseickel@gmail.com>
2022-04-03 20:46:20 +0000

    fix: refactor gig_ignored logic to avoid using local shell, fixes #226

--- lua/neo-tree/git/ignored.lua
+++ lua/neo-tree/git/ignored.lua
@@ -5,77 +5,6 @@ local git_utils = require("neo-tree.git.utils")
 local M = {}
 local sep = utils.path_separator
 
-local load_ignored_per_directory_internal = function(path, pattern)
-  if type(path) ~= "string" then
-    log.error("load_ignored_per_directory: path must be a string")
-    return {}
-  end
-  local esc_path = vim.fn.shellescape(path)
-  local cmd = string.format("git -C %s check-ignore %s%s%s", esc_path, esc_path, sep, pattern)
-  local result = vim.fn.systemlist(cmd)
-  if vim.v.shell_error == 128 then
-    if type(result) == "table" then
-      if vim.startswith(result[1], "fatal:") then
-        -- These errors are all about not being in a repository
-        return {}
-      end
-    end
-    log.error("Failed to load ignored files for ", path, ": ", result)
-    return {}
-  end
-
-  --check-ignore does not indicate directories the same as 'status' so we need to
-  --add the trailing slash to the path manually.
-  for i, item in ipairs(result) do
-    local stat = vim.loop.fs_stat(item)
-    if stat and stat.type == "directory" then
-      result[i] = item .. sep
-    end
-  end
-  return result
-end
-
-M.load_ignored_per_directory = function(path)
-  local ignored = load_ignored_per_directory_internal(path, "*")
-  -- we need to load hidden separately because I can't find a way to get both in one call
-  local hidden = load_ignored_per_directory_internal(path, ".*")
-  vim.list_extend(ignored, hidden)
-  return ignored
-end
-
-M.load_ignored = function(path)
-  local git_root = git_utils.get_repository_root(path)
-  if not git_root then
-    return {}
-  end
-  local ok, result = utils.execute_command(
-    "git --no-optional-locks status --porcelain=v1 --ignored=matching --untracked-files=normal"
-  )
-  if not ok then
-    return {}
-  end
-
-  local ignored = {}
-  for _, v in ipairs(result) do
-    -- git ignore format:
-    -- !! path/to/file
-    -- !! path/to/path/
-    -- with paths relative to the repository root
-    if v:sub(1, 2) == "!!" then
-      local entry = v:sub(4)
-      -- remove any " due to whitespace in the path
-      entry = entry:gsub('^"', ""):gsub('$"', "")
-      if utils.is_windows then
-        entry = utils.windowize_path(entry)
-      end
-      -- use the absolute path
-      table.insert(ignored, utils.path_join(git_root, entry))
-    end
-  end
-
-  return ignored
-end
-
 M.is_ignored = function(ignored, path, _type)
   path = _type == "directory" and (path .. sep) or path
   for _, v in ipairs(ignored) do
@@ -91,6 +20,84 @@ M.is_ignored = function(ignored, path, _type)
       end
     end
   end
+end
+
+local git_root_cache = {
+  known_roots = {},
+  dir_lookup = {},
+}
+local get_root_for_item = function(item)
+  local dir = item.type == "directory" and item.path or item.parent_path
+  if type(git_root_cache.dir_lookup[dir]) ~= "nil" then
+    return git_root_cache.dir_lookup[dir]
+  end
+  for _, root in ipairs(git_root_cache.known_roots) do
+    if vim.startswith(dir, root) then
+      git_root_cache.dir_lookup[dir] = root
+      return root
+    end
+  end
+  local root = git_utils.get_repository_root(dir)
+  if root then
+    git_root_cache.dir_lookup[dir] = root
+    table.insert(git_root_cache.known_roots, root)
+  else
+    git_root_cache.dir_lookup[dir] = false
+  end
+  return root
+end
+
+M.mark_ignored = function(state, items)
+  local git_roots = {}
+  for _, item in ipairs(items) do
+    local root = get_root_for_item(item)
+    if root then
+      if not git_roots[root] then
+        git_roots[root] = {}
+      end
+      table.insert(git_roots[root], item.path)
+    end
+  end
+
+  local all_results = {}
+  for repo_root, repo_items in pairs(git_roots) do
+    local cmd = {"git", "-C", repo_root, "check-ignore"}
+    for _, item in ipairs(repo_items) do
+      table.insert(cmd, item)
+    end
+    local result = vim.fn.systemlist(cmd)
+    if vim.v.shell_error == 128 then
+      if type(result) == "table" then
+        if vim.startswith(result[1], "fatal:") then
+          -- These errors are all about not being in a repository
+          log.error("Error in git.mark_ignored: ", result[1])
+          result = {}
+        end
+      end
+      log.error("Failed to load ignored files for", state.path, ":", result)
+      result = {}
+    end
+
+    --check-ignore does not indicate directories the same as 'status' so we need to
+    --add the trailing slash to the path manually.
+    for i, item in ipairs(result) do
+      local stat = vim.loop.fs_stat(item)
+      if stat and stat.type == "directory" then
+        result[i] = item .. sep
+      end
+    end
+    vim.list_extend(all_results, result)
+  end
+
+
+  for _, item in ipairs(items) do
+    if M.is_ignored(all_results, item.path, item.type) then
+      item.filtered_by = item.filtered_by or {}
+      item.filtered_by.gitignored = true
+    end
+  end
+
+  return all_results
 end
 
 return M
