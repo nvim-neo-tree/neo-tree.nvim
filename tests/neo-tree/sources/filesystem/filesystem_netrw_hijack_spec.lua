5b11e29af8474a26e2ffc3f011eb86451b60bb1b
Munif Tanjim <hello@muniftanjim.dev>
2022-07-15 04:58:36 +0600

    test: refactor hijack_netrw_behavior tests

--- tests/neo-tree/sources/filesystem/filesystem_netrw_hijack_spec.lua
+++ tests/neo-tree/sources/filesystem/filesystem_netrw_hijack_spec.lua
@@ -8,35 +8,41 @@ describe("Filesystem netrw hijack", function()
     u.clear_environment()
   end)
 
-  require("neo-tree").setup({
-    filesystem = {
-      hijack_netrw_behavior = "disabled",
-      window = {
-        position = "left",
-      },
-    },
-  })
   it("does not interfere with netrw when disabled", function()
+    require("neo-tree").setup({
+      filesystem = {
+        hijack_netrw_behavior = "disabled",
+        window = {
+          position = "left",
+        },
+      },
+    })
+
     vim.cmd("edit .")
+
     assert(#vim.api.nvim_list_wins() == 1, "there should only be one window")
+
     verify.after(100, function()
       local name = vim.api.nvim_buf_get_name(0)
       return name ~= "neo-tree filesystem [1]"
     end, "the buffer should not be neo-tree")
   end)
 
-  local file = "Makefile"
-  vim.cmd("edit " .. file)
-  require("neo-tree").setup({
-    filesystem = {
-      hijack_netrw_behavior = "open_default",
-      window = {
-        position = "left",
-      },
-    },
-  })
   it("opens in sidebar when behavior is open_default", function()
+    local file = "Makefile"
+    vim.cmd("edit " .. file)
+
+    require("neo-tree").setup({
+      filesystem = {
+        hijack_netrw_behavior = "open_default",
+        window = {
+          position = "left",
+        },
+      },
+    })
+
     vim.cmd("edit .")
+
     verify.eventually(200, function()
       return #vim.api.nvim_list_wins() == 2
     end, "there should be two windows")
@@ -55,22 +61,27 @@ describe("Filesystem netrw hijack", function()
     end, file .. " is not at window 2")
   end)
 
-  vim.cmd("edit " .. file)
-  vim.cmd("wincmd o")
-  require("neo-tree").setup({
-    filesystem = {
-      hijack_netrw_behavior = "open_current",
-    },
-  })
   it("opens in in splits when behavior is open_current", function()
+    local file = "Makefile"
+    vim.cmd("edit " .. file)
+
+    require("neo-tree").setup({
+      filesystem = {
+        hijack_netrw_behavior = "open_current",
+      },
+    })
+
     assert(#vim.api.nvim_list_wins() == 1, "Test should start with one window")
+
     vim.cmd("edit .")
+
     verify.eventually(200, function()
       assert(#vim.api.nvim_list_wins() == 1, "`edit .` should not open a new window")
       return vim.api.nvim_buf_get_option(0, "filetype") == "neo-tree"
     end, "neotree is not the only window")
 
     vim.cmd("split .")
+
     verify.eventually(200, function()
       if #vim.api.nvim_list_wins() ~= 2 then
         return false
