49862e2bd4d65a87aa1d19a2c7e02d88bdb92e06
HAO, Yun (郝运) <haoyun_public@163.com>
2022-10-22 04:37:10 +0200

    fix formatting

--- lua/neo-tree/sources/common/components.lua
+++ lua/neo-tree/sources/common/components.lua
@@ -353,14 +353,14 @@ M.indent = function(config, node, state)
   local level = node.level
   local with_markers = config.with_markers
   local with_expanders = config.with_expanders == nil and file_nesting.is_enabled()
-      or config.with_expanders
+    or config.with_expanders
   local marker_highlight = config.highlight or highlights.INDENT_MARKER
   local expander_highlight = config.expander_highlight or config.highlight or highlights.EXPANDER
 
   local function get_expander()
     if with_expanders and utils.is_expandable(node) then
       return node:is_expanded() and (config.expander_expanded or "")
-          or (config.expander_collapsed or "")
+        or (config.expander_collapsed or "")
     end
   end
 
