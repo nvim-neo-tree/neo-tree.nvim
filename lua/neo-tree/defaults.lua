c7aece33ab322607cba6a88bf19b13bbb241d1f3
pysan3 <pysan3@gmail.com>
2022-10-24 17:18:47 +0900

    add auto_expand_width to default config

--- lua/neo-tree/defaults.lua
+++ lua/neo-tree/defaults.lua
@@ -287,6 +287,7 @@ local config = {
     position = "left", -- left, right, top, bottom, float, current
     width = 40, -- applies to left and right positions
     height = 15, -- applies to top and bottom positions
+    auto_expand_width = false, -- expand the window when file exceeds the window width. does not work with position = "float"
     popup = { -- settings that apply to float position only
       size = {
         height = "80%",
