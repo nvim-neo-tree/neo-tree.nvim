298f6ce2301f80c30ef9259105300d13b2fe3834
Ben Plante <ben.plante123@gmail.com>
2023-04-15 14:39:16 -0400

    fix: set correct winid when window.position = current

--- lua/neo-tree/setup/netrw.lua
+++ lua/neo-tree/setup/netrw.lua
@@ -5,14 +5,16 @@ local command = require("neo-tree.command")
 local M = {}
 
 local get_position = function(source_name)
-  local pos = utils.get_value(M, "config." .. source_name .. ".window.position", "left")
+  local nt = require("neo-tree")
+  local default_pos = utils.get_value(nt.config, "window.position", "left", true)
+  local pos = utils.get_value(nt.config, source_name .. ".window.position", default_pos, true)
   return pos
 end
 
 M.get_hijack_netrw_behavior = function()
   local nt = require("neo-tree")
   local option = "filesystem.hijack_netrw_behavior"
-  local hijack_behavior = utils.get_value(nt.config, option, "open_default")
+  local hijack_behavior = utils.get_value(nt.config, option, "open_default", true)
   if hijack_behavior == "disabled" then
     return hijack_behavior
   elseif hijack_behavior == "open_default" then
