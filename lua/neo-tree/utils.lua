bc57a936d14862d2352bb841802148f0bc79c5ab
Nhat Vo <dnhatvo@icloud.com>
2023-04-15 16:58:52 +0200

    feat: added highlight for open but unloaded files (#856)

--- lua/neo-tree/utils.lua
+++ lua/neo-tree/utils.lua
@@ -257,13 +257,14 @@ end
 M.get_opened_buffers = function()
   local opened_buffers = {}
   for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
-    if vim.api.nvim_buf_is_loaded(buffer) and vim.fn.buflisted(buffer) then
+    if vim.fn.buflisted(buffer) ~= 0 then
       local buffer_name = vim.api.nvim_buf_get_name(buffer)
       if buffer_name == nil or buffer_name == "" then
         buffer_name = "[No Name]#" .. buffer
       end
       opened_buffers[buffer_name] = {
         ["modified"] = vim.api.nvim_buf_get_option(buffer, "modified"),
+        ["loaded"] = vim.api.nvim_buf_is_loaded(buffer),
       }
     end
   end
