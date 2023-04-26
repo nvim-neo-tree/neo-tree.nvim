bc79129859b8b0723a1e3ca0e6e35cb2bbb65cea
William Hou <83525937+williamhCode@users.noreply.github.com>
2023-04-26 07:40:30 -0400

    fix: error with toggle_auto_expand_width when window.width is not lua number (#900)

--- lua/neo-tree/sources/common/commands.lua
+++ lua/neo-tree/sources/common/commands.lua
@@ -192,9 +192,10 @@ M.toggle_auto_expand_width = function(state)
     return
   end
   state.window.auto_expand_width = state.window.auto_expand_width == false
+  local width = utils.resolve_width(state.window.width)
   if not state.window.auto_expand_width then
-    if (state.window.last_user_width or state.window.width) >= vim.api.nvim_win_get_width(0) then
-      state.window.last_user_width = state.window.width
+    if (state.window.last_user_width or width) >= vim.api.nvim_win_get_width(0) then
+      state.window.last_user_width = width
     end
     vim.api.nvim_win_set_width(0, state.window.last_user_width)
     state.win_width = state.window.last_user_width
