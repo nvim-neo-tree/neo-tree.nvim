dac36011fdcadc6fc4fde9eb8196febf0bd6106b
Teddytrombone <github@tmk-stgeorgen.at>
2023-10-15 18:30:09 +0200

    Refactoring

--- lua/neo-tree/sources/common/file-nesting.lua
+++ lua/neo-tree/sources/common/file-nesting.lua
@@ -6,8 +6,82 @@ local globtopattern = require("neo-tree.sources.filesystem.lib.globtopattern")
 -- File nesting a la JetBrains (#117).
 local M = {}
 M.config = {}
-M.exts = {}
-M.pattern = {}
+
+local pattern_matcher = {
+  enabled = false,
+  config = {},
+}
+
+local extension_matcher = {
+  enabled = false,
+  config = {},
+}
+
+local matchers = {}
+matchers.pattern = pattern_matcher
+matchers.exts = extension_matcher
+
+extension_matcher.can_have_nesting = function(item)
+  return utils.truthy(extension_matcher.config[item.exts])
+end
+
+extension_matcher.get_parent = function(item)
+  for base_exts, nesting_exts in pairs(extension_matcher.config) do
+    for _, exts in ipairs(nesting_exts) do
+      if item.exts == exts then
+        local parent_id = utils.path_join(item.parent_path, item.base) .. "." .. base_exts
+        if Path:new(parent_id):exists() then
+          return parent_id
+        end
+      end
+    end
+  end
+  return nil
+end
+
+pattern_matcher.can_have_nesting = function(item)
+  for _, patternFiles in pairs(pattern_matcher.config) do
+    if item.name:match(patternFiles["pattern"]) then
+      return true
+    end
+  end
+  return false
+end
+
+pattern_matcher.get_parent = function(item)
+  local dir = vim.loop.fs_scandir(item.parent_path)
+  if dir then
+    local file = vim.loop.fs_scandir_next(dir)
+    while file do
+      local stat = vim.loop.fs_stat(utils.path_join(item.parent_path, file))
+      if stat ~= nil and stat.type == "file" then
+        if file ~= item.name then
+          for _, ruleConfig in pairs(pattern_matcher.config) do
+            local pattern = ruleConfig["pattern"]
+            local item_name = item.name
+            if ruleConfig["ignore_case"] then
+              item_name = item_name:lower()
+            end
+            if file:match(pattern) then
+              for _, patternFile in pairs(ruleConfig["files"] or {}) do
+                if ruleConfig["ignore_case"] then
+                  patternFile = patternFile:lower()
+                end
+                local result = globtopattern.globtopattern(file:gsub(pattern, patternFile))
+
+                if item_name:match(result) then
+                  return utils.path_join(item.parent_path, file)
+                end
+              end
+            end
+          end
+        end
+      end
+      file = vim.loop.fs_scandir_next(dir)
+    end
+  end
+  return nil
+end
 
 --- Checks if file-nesting module is enabled by config
 ---@return boolean
@@ -36,46 +110,11 @@ function M.get_parent(item)
   if item.type ~= "file" then
     return nil
   end
-  local dir = vim.loop.fs_scandir(item.parent_path)
-  if dir then
-    local file = vim.loop.fs_scandir_next(dir)
-    while file do
-      local stat = vim.loop.fs_stat(utils.path_join(item.parent_path, file))
-      if stat ~= nil and stat.type == "file" then
-        if file ~= item.name then
-          for _, ruleConfig in pairs(M.pattern) do
-            local pattern = ruleConfig["pattern"]
-            local item_name = item.name
-            if ruleConfig["ignore_case"] then
-              pattern = case_insensitive_pattern(pattern)
-              item_name = item_name:lower()
-            end
-            if file:match(pattern) then
-              for _, patternFile in pairs(ruleConfig["files"] or {}) do
-                if ruleConfig["ignore_case"] then
-                  patternFile = patternFile:lower()
-                end
-                local result = globtopattern.globtopattern(file:gsub(pattern, patternFile))
-
-                if item_name:match(result) then
-                  return utils.path_join(item.parent_path, file)
-                end
-              end
-            end
-          end
-        end
-      end
-      file = vim.loop.fs_scandir_next(dir)
-    end
-  end
-
-  for base_exts, nesting_exts in pairs(M.exts) do
-    for _, exts in ipairs(nesting_exts) do
-      if item.exts == exts then
-        local parent_id = utils.path_join(item.parent_path, item.base) .. "." .. base_exts
-        if Path:new(parent_id):exists() then
-          return parent_id
-        end
+  for _, matcher in pairs(matchers) do
+    if matcher.enabled then
+      local parent = matcher.get_parent(item)
+      if parent ~= nil then
+        return parent
       end
     end
   end
@@ -86,17 +125,15 @@ end
 --- Checks if `item` have a valid nesting lookup
 ---@return boolean
 function M.can_have_nesting(item)
-  for _, patternFiles in pairs(M.pattern) do
-    local pattern = patternFiles["pattern"]
-    if patternFiles["ignore_case"] then
-      pattern = case_insensitive_pattern(pattern)
-    end
-    if item.name:match(pattern) then
-      return true
+  for _, matcher in pairs(matchers) do
+    if matcher.enabled then
+      if matcher.can_have_nesting(item) then
+        return true
+      end
     end
   end
 
-  return utils.truthy(M.exts[item.exts])
+  return false
 end
 
 --- Checks if `target` should be nested into `base`
@@ -112,20 +149,23 @@ end
 ---Setup the module with the given config
 ---@param config table
 function M.setup(config)
-  local newConfig = {
-    exts = {},
-    pattern = {},
-  }
   for key, value in pairs(config or {}) do
     local type = "exts"
     if value["pattern"] ~= nil then
       type = "pattern"
+      if value["ignore_case"] == true then
+        value["pattern"] = case_insensitive_pattern(value["pattern"])
+      end
     end
-    newConfig[type][key] = value
+    matchers[type]["config"][key] = value
+  end
+  local next = next
+  for key, value in pairs(matchers) do
+    if next(value.config) ~= nil then
+      value.enabled = true
+    end
   end
   M.config = config or {}
-  M.exts = newConfig["exts"]
-  M.pattern = newConfig["pattern"]
 end
 
 return M
