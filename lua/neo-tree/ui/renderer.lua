a3bc0fad5b31ccaf165ed68d168a326e1940f46d
Micah Halter <micah@mehalter.com>
2023-10-28 09:55:46 -0400

    feat(renderer): include `desc` for mappings created with function name

--- lua/neo-tree/ui/renderer.lua
+++ lua/neo-tree/ui/renderer.lua
@@ -834,6 +834,7 @@ local set_buffer_mappings = function(state)
         end
         if type(func) == "string" then
           resolved_mappings[cmd] = { text = func }
+          map_options.desc = func
           vfunc = state.commands[func .. "_visual"]
           func = state.commands[func]
         elseif type(func) == "function" then
