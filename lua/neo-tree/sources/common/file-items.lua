9298c1b7a52e63ed4fd977fd792a0d4afe9260d5
yehuohan <yehuohan@qq.com>
2023-12-04 21:38:56 +0800

    fix(filesystem): use getftype to check symlink on windows (#1252)
    Co-authored-by: cseickel <cseickel@gmail.com>

--- lua/neo-tree/sources/common/file-items.lua
+++ lua/neo-tree/sources/common/file-items.lua
@@ -117,6 +117,11 @@ function create_item(context, path, _type, bufnr)
     path = path,
     type = _type,
   }
+  if utils.is_windows then
+    if vim.fn.getftype(path) == "link" then
+      item.type = "link"
+    end
+  end
   if item.type == "link" then
     item.is_link = true
     item.link_to = vim.loop.fs_realpath(path)
