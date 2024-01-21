a4d146de782e3b8112129d9613bec6f927e5c417
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2024-01-21 16:55:26 +0100

    Fix hijack for position=current

--- lua/neo-tree/sources/common/hijack_cursor.lua
+++ lua/neo-tree/sources/common/hijack_cursor.lua
@@ -1,5 +1,6 @@
 local events = require("neo-tree.events")
 local log = require("neo-tree.log")
+local manager = require("neo-tree.sources.manager")
 
 local M = {}
 
@@ -12,21 +13,24 @@ local hijack_cursor_handler = function()
         log.debug("Cursor hijack failure: " .. vim.inspect(source))
         return
     end
-    local state = require("neo-tree.sources.manager").get_state(source)
+    local winid = nil
+    local _, position = pcall(vim.api.nvim_buf_get_var, 0, "neo_tree_position")
+    if position == "current" then
+      winid = vim.api.nvim_get_current_win()
+    end
+
+    local state = manager.get_state(source, nil, winid)
     if state == nil then
       return
     end
-    local winid = state.winid
-    if vim.api.nvim_get_current_win() == winid then
-      local node = state.tree:get_node()
-      log.debug("Cursor moved in tree window, hijacking cursor position")
-      local cursor = vim.api.nvim_win_get_cursor(0)
-      local row = cursor[1]
-      local current_line = vim.api.nvim_get_current_line()
-      local startIndex, _ = string.find(current_line, node.name, nil, true)
-      if startIndex then
-        vim.api.nvim_win_set_cursor(0, { row, startIndex - 1 })
-      end
+    local node = state.tree:get_node()
+    log.debug("Cursor moved in tree window, hijacking cursor position")
+    local cursor = vim.api.nvim_win_get_cursor(0)
+    local row = cursor[1]
+    local current_line = vim.api.nvim_get_current_line()
+    local startIndex, _ = string.find(current_line, node.name, nil, true)
+    if startIndex then
+      vim.api.nvim_win_set_cursor(0, { row, startIndex - 1 })
     end
 end
 
