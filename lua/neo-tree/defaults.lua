4cfcbe2cc27b6324e501c96be8dd30751e0c209e
pysan3 <pysan3@gmail.com>
2024-02-05 21:36:40 +0900

    refactor(defaults): move event_handler examples to wiki

--- lua/neo-tree/defaults.lua
+++ lua/neo-tree/defaults.lua
@@ -511,102 +511,21 @@ config.document_symbols = {
     Event = { icon = "", hl = "Constant" },
     Operator = { icon = "󰆕", hl = "Operator" },
     TypeParameter = { icon = "󰊄", hl = "Type" },
+    TypeAlias = { icon = " ", hl = "Type" },
+    Parameter = { icon = " ", hl = "@parameter" },
+    StaticMethod = { icon = "󰠄 ", hl = "Function" },
+    Macro = { icon = " ", hl = "Macro" },
   },
-  custom_kinds = {},
+  custom_kinds = {
+    [252] = "TypeAlias",
+    [253] = "Parameter",
+    [254] = "StaticMethod",
+    [255] = "Macro",
+  },
 }
 
 ---@type NeotreeConfig.event_handler[]
--- TODO: Move event_handler examples to wiki
--- See http://TODO/make/wiki/page for examples.
+---@see https://github.com/nvim-neo-tree/neo-tree.nvim/wiki/Recipes#events for examples.
 config.event_handlers = {}
-
--- config.event_handlers = {
---   {
---     event = "before_render",
---     handler = function(state)
---       -- add something to the state that can be used by custom components
---     end,
---   },
---   {
---     event = "file_opened",
---     handler = function(file_path)
---       --auto close
---       require("neo-tree.command").execute({ action = "close" })
---     end,
---   },
---   {
---     event = "file_opened",
---     handler = function(file_path)
---       --clear search after opening a file
---       require("neo-tree.sources.filesystem").reset_search()
---     end,
---   },
---   {
---     event = "file_renamed",
---     handler = function(args)
---       -- fix references to file
---       print(args.source, " renamed to ", args.destination)
---     end,
---   },
---   {
---     event = "file_moved",
---     handler = function(args)
---       -- fix references to file
---       print(args.source, " moved to ", args.destination)
---     end,
---   },
---   {
---     event = "neo_tree_buffer_enter",
---     handler = function()
---       vim.cmd("highlight! Cursor blend=100")
---     end,
---   },
---   {
---     event = "neo_tree_buffer_leave",
---     handler = function()
---       vim.cmd("highlight! Cursor guibg=#5f87af blend=0")
---     end,
---   },
---   {
---     event = "neo_tree_window_before_open",
---     handler = function(args)
---       print("neo_tree_window_before_open", vim.inspect(args))
---     end,
---   },
---   {
---     event = "neo_tree_window_after_open",
---     handler = function(args)
---       vim.cmd("wincmd =")
---     end,
---   },
---   {
---     event = "neo_tree_window_before_close",
---     handler = function(args)
---       print("neo_tree_window_before_close", vim.inspect(args))
---     end,
---   },
---   {
---     event = "neo_tree_window_after_close",
---     handler = function(args)
---       vim.cmd("wincmd =")
---     end,
---   },
--- }
-
--- TODO: Add this hack to wiki.
--- kinds = {
---   -- ccls
---   -- TypeAlias = { icon = ' ', hl = 'Type' },
---   -- Parameter = { icon = ' ', hl = '@parameter' },
---   -- StaticMethod = { icon = '󰠄 ', hl = 'Function' },
---   -- Macro = { icon = ' ', hl = 'Macro' },
--- }
--- custom_kinds = {
---   -- ccls
---   [252] = 'TypeAlias',
---   [253] = 'Parameter',
---   [254] = 'StaticMethod',
---   [255] = 'Macro',
--- }
 
 return config
