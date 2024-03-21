934935a704d414b09f4cd088f085eac517db4943
pysan3 <pysan3@gmail.com>
2024-03-22 00:31:06 +0900

    fix(renderer): don't close buffer when toggled on

--- lua/neo-tree/ui/renderer.lua
+++ lua/neo-tree/ui/renderer.lua
@@ -163,13 +163,15 @@ M.close = function(state, focus_prior_window)
     end
     state.winid = nil
   end
-  local bufnr = utils.get_value(state, "bufnr", 0, true)
-  state.bufnr = nil
-  vim.schedule(function()
-    if bufnr > 0 and vim.api.nvim_buf_is_valid(bufnr) then
-      vim.api.nvim_buf_delete(bufnr, { force = true })
-    end
-  end)
+  if window_existed then
+    local bufnr = utils.get_value(state, "bufnr", 0, true)
+    state.bufnr = nil
+    vim.schedule(function()
+      if bufnr > 0 and vim.api.nvim_buf_is_valid(bufnr) then
+        vim.api.nvim_buf_delete(bufnr, { force = true })
+      end
+    end)
+  end
   return window_existed
 end
 
