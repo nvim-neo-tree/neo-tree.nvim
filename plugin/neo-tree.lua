5c0b6cb8228a781e80fe059e0142a98257b73826
pynappo <lehtien.david@gmail.com>
2025-01-16 03:54:08 -0800

    revert to custom instead of customlist

--- plugin/neo-tree.lua
+++ plugin/neo-tree.lua
@@ -2,13 +2,12 @@ if vim.g.loaded_neo_tree == 1 or vim.g.loaded_neo_tree == true then
   return
 end
 
+-- Possibly convert this to lua using customlist instead of custom in the future?
 vim.api.nvim_create_user_command("Neotree", function(ctx)
   require("neo-tree.command")._command(unpack(ctx.fargs))
 end, {
   nargs = "*",
-  complete = function(argLead, cmdLine)
-    require("neo-tree.command").complete_args(argLead, cmdLine)
-  end,
+  complete = "custom,v:lua.require'neo-tree.command'.complete_args",
 })
 
 ---@param path string? The path to check
