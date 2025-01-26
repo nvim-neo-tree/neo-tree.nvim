521bb4253b1adc2a1ff6c584bc839eba9bc99c0b
pynappo <pynappo@proton.me>
2025-01-26 06:27:24 -0800

    fix(setup): defer hijack until setup or BufEnter (#1667)

--- plugin/neo-tree.lua
+++ plugin/neo-tree.lua
@@ -17,30 +17,28 @@ local function try_netrw_hijack(path)
     return false
   end
 
+  local stats = (vim.uv or vim.loop).fs_stat(path)
+  if not stats or stats.type ~= "directory" then
+    return false
+  end
+
   local netrw = require("neo-tree.setup.netrw")
   if netrw.get_hijack_behavior() ~= "disabled" then
     vim.cmd("silent! autocmd! FileExplorer *")
-    local stats = (vim.uv or vim.loop).fs_stat(path)
-    if stats and stats.type == "directory" then
-      return netrw.hijack()
-    end
+    return netrw.hijack()
   end
   return false
 end
 
--- currently need to check first arg to not break hijacked on
--- configs that already lazy-load neo-tree (e.g. lazyvim)
-local first_arg = vim.fn.argv(0) --[[@as string]]
-if not try_netrw_hijack(first_arg) then
-  local augroup = vim.api.nvim_create_augroup("NeoTree_NetrwDeferred", { clear = true })
-  vim.api.nvim_create_autocmd("BufEnter", {
-    group = augroup,
-    callback = function(args)
-      if try_netrw_hijack(args.file) then
-        vim.api.nvim_del_augroup_by_id(augroup)
-      end
-    end,
-  })
-end
+local augroup = vim.api.nvim_create_augroup("NeoTree_NetrwDeferred", { clear = true })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = augroup,
+  callback = function(args)
+    if try_netrw_hijack(args.file) then
+      vim.api.nvim_del_augroup_by_id(augroup)
+    end
+  end,
+})
 
 vim.g.loaded_neo_tree = 1
