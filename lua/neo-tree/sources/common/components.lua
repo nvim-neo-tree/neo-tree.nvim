1fe61429170ea971b62fd62f1fa06162ba09dd25
Zack Ruppert <dev@zack.rupinga.com>
2025-02-20 12:38:27 -0500

    Only add sort icon in window mode

--- lua/neo-tree/sources/common/components.lua
+++ lua/neo-tree/sources/common/components.lua
@@ -344,7 +344,7 @@ M.name = function(config, node, state)
 
   if node:get_depth() == 1 and node.type ~= "message" then
     highlight = highlights.ROOT_NAME
-    if state.sort and state.sort.label == "Name" then
+    if state.current_position == "current" and state.sort and state.sort.label == "Name" then
       local icon = state.sort.direction == 1 and "▲" or "▼"
       text = text .. "  " .. icon
     end
