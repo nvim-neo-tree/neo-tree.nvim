b840de58e32788e66c2efaabc25fd3f3acc99ffb
pynappo <pynappo@proton.me>
2025-02-25 03:14:14 -0800

    windowize path instead of using utils.normalize_path
    While utils.normalize_path ought to just normalize paths on linux too,
    this minimizes possible regressions in behavior until a future, more
    thorough PR can be done.

--- lua/neo-tree/sources/filesystem/lib/fs_actions.lua
+++ lua/neo-tree/sources/filesystem/lib/fs_actions.lua
@@ -181,7 +181,10 @@ M.move_node = function(source, destination, callback, using_root_directory)
   local _, name = utils.split_path(source)
   get_unused_name(destination or source, using_root_directory, function(dest)
     -- Resolve user-inputted relative paths out of the absolute paths
-    dest = utils.normalize_path(dest)
+    dest = vim.fs.normalize(dest)
+    if utils.is_windows then
+      utils.windowize_path(dest)
+    end
     local function move_file()
       create_all_parents(dest)
       loop.fs_rename(source, dest, function(err)
