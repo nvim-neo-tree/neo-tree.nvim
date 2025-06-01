f3e8633f06007e015f855d3c1ec0cee23af14d8e
pynappo <pynappo@proton.me>
2025-06-01 01:50:32 -0700

    fix(fs_actions): disallow deletion of root nodes (#1793)

--- lua/neo-tree/sources/common/commands.lua
+++ lua/neo-tree/sources/common/commands.lua
@@ -645,16 +645,35 @@ end
 M.delete = function(state, callback)
   local tree = state.tree
   local node = tree:get_node()
-  if node.type == "file" or node.type == "directory" then
-    fs_actions.delete_node(node.path, callback)
-  else
+  if node.type ~= "file" and node.type ~= "directory" then
     log.warn("The `delete` command can only be used on files and directories")
+    return
   end
+  if node:get_depth() == 1 then
+    log.error(
+      "Will not delete root node "
+        .. node.path
+        .. ", please back out of the current directory if you want to delete the root node."
+    )
+    return
+  end
+  fs_actions.delete_node(node.path, callback)
 end
 
+---@param selected_nodes NuiTree.Node[]
+---@param callback function
 M.delete_visual = function(state, selected_nodes, callback)
   local paths_to_delete = {}
   for _, node_to_delete in pairs(selected_nodes) do
+    if node_to_delete:get_depth() == 1 then
+      log.error(
+        "Will not delete root node "
+          .. node_to_delete.path
+          .. ", please back out of the current directory if you want to delete the root node."
+      )
+      return
+    end
+
     if node_to_delete.type == "file" or node_to_delete.type == "directory" then
       table.insert(paths_to_delete, node_to_delete.path)
     end
--- lua/neo-tree/ui/inputs.lua
+++ lua/neo-tree/ui/inputs.lua
@@ -76,25 +76,33 @@ M.input = function(message, default_value, callback, options, completion)
   end
 end
 
+---Blocks if callback is omitted
+---@param message string
+---@param callback? fun(confirmed: boolean)
+---@return boolean? confirmed_if_no_callback
 M.confirm = function(message, callback)
-  if nt.config.use_popups_for_input then
-    local popup_options = popups.popup_options(message, 10)
+  if callback then
+    if nt.config.use_popups_for_input then
+      local popup_options = popups.popup_options(message, 10)
 
-    ---@class NuiInput
-    local input = NuiInput(popup_options, {
-      prompt = " y/n: ",
-      on_close = function()
-        callback(false)
-      end,
-      on_submit = function(value)
-        callback(value == "y" or value == "Y")
-      end,
-    })
+      ---@class NuiInput
+      local input = NuiInput(popup_options, {
+        prompt = " y/n: ",
+        on_close = function()
+          callback(false)
+        end,
+        on_submit = function(value)
+          callback(value == "y" or value == "Y")
+        end,
+      })
 
-    input.prompt_type = "confirm"
-    M.show_input(input)
+      input.prompt_type = "confirm"
+      M.show_input(input)
+    else
+      callback(vim.fn.confirm(message, "&Yes\n&No") == 1)
+    end
   else
-    callback(vim.fn.confirm(message, "&Yes\n&No") == 1)
+    return vim.fn.confirm(message, "&Yes\n&No") == 1
   end
 end
 
