44d3f1bf45de443d593765b0d074e3f272bb3159
pynappo <pynappo@proton.me>
2025-07-02 09:44:55 -0700

    fix root

--- tests/mininit.lua
+++ tests/mininit.lua
@@ -1,7 +1,7 @@
-local root_dir = vim.fs.find("plugin", { upward = true, limit = 1 })[1]
+local root_dir = vim.fs.find("neo-tree.nvim", { upward = true, limit = 1 })[1]
 assert(root_dir, "no neo-tree found")
 
-vim.opt.packpath:prepend(root_dir .. ".dependencies")
+vim.opt.packpath:prepend(root_dir .. "/.dependencies")
 
 vim.opt.rtp = {
   root_dir,
