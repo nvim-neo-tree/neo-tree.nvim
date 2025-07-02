141a58144e6346c91e88dcba0c8b5e15e469d7ee
pynappo <pynappo@proton.me>
2025-07-02 09:54:00 -0700

    fix tests

--- tests/mininit.lua
+++ tests/mininit.lua
@@ -1,9 +1,13 @@
 local root_dir = vim.fs.find("neo-tree.nvim", { upward = true, limit = 1 })[1]
 assert(root_dir, "no neo-tree found")
 
+package.path = ("%s;%s/?.lua;%s/?/init.lua"):format(package.path, root_dir, root_dir)
 vim.opt.packpath:prepend(root_dir .. "/.dependencies")
 
 vim.opt.rtp = {
   root_dir,
   vim.env.VIMRUNTIME,
 }
+
+-- need this for tests to work
+vim.cmd.source(root_dir .. "/plugin/neo-tree.lua")
