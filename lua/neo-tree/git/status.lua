562cd1ed18a55f44a334857e4733877ff6b08823
pynappo <pynappo@proton.me>
2025-10-08 02:30:19 -0700

    parse git status porcelain v2 instead of 3 separate cmds

--- lua/neo-tree/git/status.lua
+++ lua/neo-tree/git/status.lua
@@ -123,10 +123,11 @@ local parse_git_status_line = function(context, line)
     end)
   end
 end
+
 ---Parse "git status" output for the current working directory.
----@base git ref base
----@exclude_directories boolean Whether to skip bubling up status to directories
----@path string Path to run the git status command in, defaults to cwd.
+---@param base string git ref base
+---@param exclude_directories boolean Whether to skip bubling up status to directories
+---@param path string Path to run the git status command in, defaults to cwd.
 ---@return neotree.git.Status, string? git_status the neotree.Git.Status of the given root
 M.status = function(base, exclude_directories, path)
   local git_root = git_utils.get_repository_root(path)
@@ -134,18 +135,160 @@ M.status = function(base, exclude_directories, path)
     return {}
   end
 
-  local C = git_root
-  local staged_cmd = { "git", "-C", C, "diff", "--staged", "--name-status", base, "--" }
+  local s1 = vim.uv.hrtime()
+  local status_cmd =
+    { "git", "-c", "status.relativePaths=true", "-C", git_root, "status", "--porcelain=v2", "-z" }
+  local status_result = vim.fn.system(status_cmd)
+
+  local status_ok = vim.v.shell_error == 0
+  if not status_ok then
+    return {}
+  end
+
+  local git_root_dir = git_root .. utils.path_separator
+  ---@type table<string, string>
+  local gs = {}
+  -- system() replaces \000 with \001
+  local status_iter = vim.gsplit(status_result, "\001", { plain = true })
+  local prev_line = ""
+  for line in status_iter do
+    -- Example status:
+    -- 1 D. N... 100644 000000 000000 ade2881afa1dcb156a3aa576024aa0fecf789191 0000000000000000000000000000000000000000 deleted_staged.txt
+    -- 1 .D N... 100644 100644 000000 9c13483e67ceff219800303ec7af39c4f0301a5b 9c13483e67ceff219800303ec7af39c4f0301a5b deleted_unstaged.txt
+    -- 1 MM N... 100644 100644 100644 4417f3aca512ffdf247662e2c611ee03ff9255cc 29c0e9846cd6410a44c4ca3fdaf5623818bd2838 modified_mixed.txt
+    -- 1 M. N... 100644 100644 100644 f784736eecdd43cd8eb665615163cfc6506fca5f 8d6fad5bd11ac45c7c9e62d4db1c427889ed515b modified_staged.txt
+    -- 1 .M N... 100644 100644 100644 c9e1e027aa9430cb4ffccccf45844286d10285c1 c9e1e027aa9430cb4ffccccf45844286d10285c1 modified_unstaged.txt
+    -- 1 A. N... 000000 100644 100644 0000000000000000000000000000000000000000 89cae60d74c222609086441e29985f959b6ec546 new_staged_file.txt
+    -- 2 R. N... 100644 100644 100644 3454a7dc6b93d1098e3c3f3ec369589412abdf99 3454a7dc6b93d1098e3c3f3ec369589412abdf99 R100 renamed_staged_new.txt
+    -- renamed_staged_old.txt
+    -- 1 .T N... 100644 100644 120000 192f10ed8c11efb70155e8eb4cae6ec677347623 192f10ed8c11efb70155e8eb4cae6ec677347623 type_change.txt
+    -- ? .gitignore
+    -- ? untracked.txt
+
+    -- 1 <XY> <sub> <mH> <mI> <mW> <hH> <hI> <path>
+    -- 2 <XY> <sub> <mH> <mI> <mW> <hH> <hI> <X><score> <path><sep><origPath>
+    local t = line:sub(1, 1)
+    if t == "1" then
+      local XY = line:sub(3, 4)
+      -- local submodule_state = line:sub(6, 9)
+      -- local mH = line:sub(11, 16)
+      -- local mI = line:sub(18, 23)
+      -- local mW = line:sub(25, 30)
+      -- local hH = line:sub(32, 71)
+      -- local hI = line:sub(73, 112)
+      local pathname = line:sub(114)
+      gs[git_root_dir .. pathname] = XY
+    elseif t == "2" then
+      -- local XY = line:sub(3, 4)
+      -- local submodule_state = line:sub(6, 9)
+      -- local mH = line:sub(11, 16)
+      -- local mI = line:sub(18, 23)
+      -- local mW = line:sub(25, 30)
+      -- local hH = line:sub(32, 71)
+      -- local hI = line:sub(73, 112)
+      local rest = line:sub(114)
+      local score_and_pathname = vim.split(rest, " ", { plain = true })
+      -- iterate over the original path
+      gs[git_root_dir .. score_and_pathname[2]] = score_and_pathname[1]
+      status_iter()
+    elseif t == "u" then
+      local XY = line:sub(3, 4)
+      -- local submodule_state = line:sub(6, 9)
+      -- local m1 = line:sub(11, 16)
+      -- local m2 = line:sub(18, 23)
+      -- local m3 = line:sub(25, 30)
+      -- local mW = line:sub(32, 37)
+      -- local h1 = line:sub(39, 78)
+      -- local h2 = line:sub(80, 119)
+      -- local h3 = line:sub(121, 160)
+      local pathname = line:sub(162)
+      gs[git_root_dir .. pathname] = XY
+    else
+      prev_line = line
+      break
+    end
+    -- X          Y     Meaning
+    -- -------------------------------------------------
+    -- 	        [AMD]   not updated
+    -- M        [ MTD]  updated in index
+    -- T        [ MTD]  type changed in index
+    -- A        [ MTD]  added to index
+    -- D                deleted from index
+    -- R        [ MTD]  renamed in index
+    -- C        [ MTD]  copied in index
+    -- [MTARC]          index and work tree matches
+    -- [ MTARC]    M    work tree changed since index
+    -- [ MTARC]    T    type changed in work tree since index
+    -- [ MTARC]    D    deleted in work tree
+    --             R    renamed in work tree
+    --             C    copied in work tree
+    -- -------------------------------------------------
+    -- D           D    unmerged, both deleted
+    -- A           U    unmerged, added by us
+    -- U           D    unmerged, deleted by them
+    -- U           A    unmerged, added by them
+    -- D           U    unmerged, deleted by us
+    -- A           A    unmerged, both added
+    -- U           U    unmerged, both modified
+    -- -------------------------------------------------
+    -- ?           ?    untracked
+    -- !           !    ignored
+    -- -------------------------------------------------
+  end
+
+  if prev_line:sub(1, 1) == "?" then
+    -- untracked
+    gs[git_root_dir .. prev_line:sub(3)] = "?"
+    for line in status_iter do
+      if line:sub(1, 1) ~= "?" then
+        prev_line = line
+        break
+      end
+      gs[git_root_dir .. prev_line:sub(3)] = "?"
+    end
+  end
+
+  -- local gitignored = {}
+  if prev_line:sub(1, 1) == "!" then
+    -- gitignored[#gitignored + 1] = prev_line:sub(3)
+    gs[git_root_dir .. prev_line:sub(3)] = "!"
+    for line in status_iter do
+      -- gitignored[#gitignored + 1] = line:sub(3)
+      gs[git_root_dir .. line:sub(3)] = "!"
+    end
+  end
+
+  -- bubble up every status
+  for dir, status in pairs(gs) do
+    for parent in utils.path_parents(dir) do
+      local parent_status = gs[parent]
+      if not parent_status then
+        gs[parent] = status
+      else
+        local better_status =
+          get_priority_git_status_code(parent_status:sub(1, 1), status:sub(1, 1))
+        if better_status == parent_status then
+          break -- stop bubbling
+        else
+          gs[parent] = better_status
+        end
+      end
+    end
+  end
+  local e1 = vim.uv.hrtime()
+
+  local s2 = vim.uv.hrtime()
+  local staged_cmd = { "git", "-C", git_root, "diff", "--staged", "--name-status", base, "--" }
   local staged_ok, staged_result = utils.execute_command(staged_cmd)
   if not staged_ok then
     return {}
   end
-  local unstaged_cmd = { "git", "-C", C, "diff", "--name-status" }
+  local unstaged_cmd = { "git", "-C", git_root, "diff", "--name-status" }
   local unstaged_ok, unstaged_result = utils.execute_command(unstaged_cmd)
   if not unstaged_ok then
     return {}
   end
-  local untracked_cmd = { "git", "-C", C, "ls-files", "--exclude-standard", "--others" }
+  local untracked_cmd = { "git", "-C", git_root, "ls-files", "--exclude-standard", "--others" }
   local untracked_ok, untracked_result = utils.execute_command(untracked_cmd)
   if not untracked_ok then
     return {}
@@ -173,6 +316,11 @@ M.status = function(base, exclude_directories, path)
     end
     parse_git_status_line(context, line)
   end
+  local e2 = vim.uv.hrtime()
+  print(e1 - s1)
+  print(vim.inspect(gs))
+  print(e2 - s2)
+  print(vim.inspect(context.git_status))
 
   return context.git_status, git_root
 end
@@ -265,9 +413,7 @@ M.status_async = function(path, base, opts)
         enable_recording = false,
         maximium_results = context.max_lines,
         on_stdout = function(err, line, job)
-          if should_process(err, line, job, "status_async staged error:") then
-            table.insert(context.lines, line)
-          end
+          table.insert(context.lines, line)
         end,
         on_stderr = function(err, line)
           if err and err > 0 then
