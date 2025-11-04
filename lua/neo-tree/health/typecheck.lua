015f85afe8607a28d1f89bb2034d9f0b6e429244
pynappo <pynappo@proton.me>
2025-11-04 00:36:10 -0800

    safe table

--- lua/neo-tree/health/typecheck.lua
+++ lua/neo-tree/health/typecheck.lua
@@ -1,4 +1,5 @@
 local M = {}
+local utils = require("neo-tree.health.utils")
 
 ---Like type() but also supports "callable" like neovim does.
 ---@see _G.type
@@ -22,110 +23,6 @@ end
 M.errfuncs = {}
 ---@type string[]
 M.namestack = {}
-
----@generic T : table
----@param path string
----@param tbl T
----@param accesses string[]
----@param missed_paths table<string, true?>
----@return T mocked_tbl
-local function mock_recursive(path, tbl, accesses, missed_paths, track_missed)
-  local mock_table = {}
-
-  ---@class neotree.health.Mock.Metatable<T> : metatable
-  ---@field accesses string[]
-  local mt = {
-    __original_table = tbl,
-    accesses = accesses,
-  }
-
-  ---@return string[] missed_paths
-  mt.get_missed_paths = function()
-    ---@type string[]
-    local missed_list = {}
-    if track_missed then
-      for p, _ in pairs(missed_paths) do
-        table.insert(missed_list, p)
-      end
-    end
-    table.sort(missed_list)
-    return missed_list
-  end
-
-  mt.__index = function(_, key)
-    local path_segment
-    if type(key) == "number" then
-      path_segment = ("[%02d]"):format(key)
-    else
-      path_segment = tostring(key)
-    end
-
-    local full_path
-    if path == "" then
-      full_path = path_segment
-    elseif type(key) == "number" then
-      full_path = path .. path_segment
-    else
-      full_path = path .. "." .. path_segment
-    end
-
-    -- Track accesses and missed accesses
-    mt.accesses[#mt.accesses + 1] = full_path
-    if track_missed then
-      missed_paths[full_path] = nil
-    end
-
-    local value = mt.__original_table[key]
-
-    if type(value) == "table" then
-      return mock_recursive(full_path, value, mt.accesses, missed_paths, track_missed)
-    end
-    return value
-  end
-
-  setmetatable(mock_table, mt)
-  return mock_table
-end
-
---- Wraps a given table in a special mock table that tracks all accesses
---- (reads) to its fields and sub-fields. Optionally tracks unaccessed fields.
----
----@generic T : table
----@param name string The base name for the table, this forms the root of the access paths.
----@param tbl T The table to be mocked.
----@param track_missed boolean? Track which fields were NOT accessed.
----@return T mocked
-function M.mock(name, tbl, track_missed)
-  local accesses = {}
-  local path_set = {}
-  track_missed = track_missed or false
-
-  if track_missed then
-    -- Generate another mock table and fully traverse that one first
-    local root_mock = M.mock(name, tbl, false)
-
-    ---@param current_table table
-    local function deep_traverse_mock(current_table)
-      ---@type neotree.health.Mock.Metatable
-      local mt = getmetatable(current_table)
-      for k, v in pairs(mt.__original_table) do
-        if type(v) == "table" then
-          deep_traverse_mock(current_table[k])
-        else
-          mt.__index(nil, k)
-        end
-      end
-    end
-    deep_traverse_mock(root_mock)
-    accesses = getmetatable(root_mock).accesses
-    for _, path in ipairs(accesses) do
-      path_set[path] = true
-    end
-  end
-
-  -- Start the recursive mocking process, passing all necessary shared tracking data.
-  return mock_recursive(name, tbl, accesses, path_set, track_missed)
-end
 
 ---A comprehensive version of vim.validate that makes it easy to validate nested tables of various types
 ---@generic T
@@ -156,7 +53,7 @@ function M.validate(name, value, validator, optional, message, on_invalid, track
       M.errfuncs[#M.errfuncs + 1] = on_invalid
     end
     if track_missed and type(value) == "table" then
-      value = M.mock(name, value, true)
+      value = utils.mock(name, value, true)
     end
     ok, valid, errinfo = pcall(validator, value)
     if on_invalid then
