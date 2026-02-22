9c17849e221c2e5682039e256bb8ea4689e42112
pynappo <pynappo@proton.me>
2026-02-22 01:51:31 -0800

    refactor ignore code to support extended paths

--- lua/neo-tree/sources/filesystem/lib/ignored.lua
+++ lua/neo-tree/sources/filesystem/lib/ignored.lua
@@ -3,15 +3,29 @@ local M = {}
 local uv = vim.uv or vim.loop
 local utils = require("neo-tree.utils")
 local glob = require("neo-tree.sources.filesystem.lib.lua-glob")
+local log = require("neo-tree.log")
 
----@class neotree.sources.filesystem.Ignore.Rule
----@field root string
----@field pattern string
----@field negate string
+---Cached tuples of mtime (ns), inode, glob
+---@type table<string, [integer, integer, neotree.lib.LuaGlob]>
+local cache = {}
 
 ---@param path string
+---@param root string
+---@param stat uv.fs_stat.result
 ---@return neotree.lib.LuaGlob
-local file_to_glob = function(path)
+local file_to_glob = function(path, root, stat)
+  local mtime_ns = stat.mtime.nsec
+  local inode = stat.ino
+
+  local cached = cache[path]
+  if cached then
+    local cached_mtime_ns, cached_inode, cached_glob = unpack(cached)
+    -- check if mtime/inode ok
+    if cached_mtime_ns == mtime_ns and cached_inode == inode then
+      return cached_glob
+    end
+  end
+
   ---@type string[]
   local lines = {}
   for line in io.lines(path) do
@@ -21,12 +35,11 @@ local file_to_glob = function(path)
     end
   end
 
-  local parent_path = assert(utils.split_path(path))
-  return glob.gitignore(lines, {
-    root = parent_path,
+  local glob_for_file = glob.gitignore(lines, {
+    root = root,
   }, {
     type = function(p)
-      local abspath = utils.path_join(parent_path, p)
+      local abspath = utils.path_join(root, p)
       local kind = assert(uv.fs_stat(abspath)).type
       if kind == "directory" then
         return "directory"
@@ -36,6 +49,29 @@ local file_to_glob = function(path)
       return nil
     end,
   })
+  cache[path] = { mtime_ns, inode, glob_for_file }
+  return glob_for_file
+end
+
+---@param relpaths string[]
+---@param dir string
+---@return neotree.sources.filesystem.Ignorer[]
+local find_ignorers_in_dir = function(relpaths, dir)
+  local found = {}
+  for _, relpath in ipairs(relpaths) do
+    local fullpath = utils.path_join(dir, relpath)
+    local lstat = uv.fs_lstat(fullpath)
+    if lstat then
+      ---@class neotree.sources.filesystem.Ignorer
+      local ignorer = {
+        fullpath,
+        file_to_glob(fullpath, dir, lstat),
+        dir,
+      }
+      found[#found + 1] = ignorer
+    end
+  end
+  return found
 end
 
 ---@param state neotree.State
@@ -43,6 +79,7 @@ end
 ---@return nil
 M.mark_ignored = function(state, items)
   local ignore_files = state.filtered_items.ignore_files
+  vim.print("mark_ignored")
   if not ignore_files or vim.tbl_isempty(ignore_files) then
     return {}
   end
@@ -50,42 +87,43 @@ M.mark_ignored = function(state, items)
   local folders = {}
 
   for _, item in ipairs(items) do
-    local folder = utils.split_path(item.path)
-    if folder then
-      folders[folder] = folders[folder] or {}
-      table.insert(folders[folder], item)
+    local parent = utils.split_path(item.path)
+    if parent then
+      folders[parent] = folders[parent] or {}
+      table.insert(folders[parent], item)
     end
   end
 
-  ---@type table<string, neotree.lib.LuaGlob>
-  local globs = {}
-
-  ---@type table<string, { path: string, parser: neotree.lib.LuaGlob }[]>
+  ---@type table<string, neotree.sources.filesystem.Ignorer[]>
   local ignorers = {}
 
   for folder, children in pairs(folders) do
-    ignorers[folder] = ignorers[folder] or {}
-    local folder_ignorers = ignorers[folder]
-    local applicable_ignore_files =
-      vim.fs.find(ignore_files, { upward = true, limit = math.huge, path = folder, type = "file" })
+    ---@type neotree.sources.filesystem.Ignorer[]
+    local applicable_ignorers = {}
+    ---@type string?
+    local parent = folder
+    while parent do
+      local ignorers_in_parent = ignorers[parent]
+      if not ignorers_in_parent then
+        ignorers_in_parent = find_ignorers_in_dir(ignore_files, parent)
+        ignorers[parent] = ignorers_in_parent
+      end
+      vim.list_extend(applicable_ignorers, ignorers_in_parent)
 
-    for _, ignore_filepath in ipairs(applicable_ignore_files) do
-      globs[ignore_filepath] = globs[ignore_filepath] or file_to_glob(ignore_filepath)
-      folder_ignorers[#folder_ignorers + 1] = {
-        path = ignore_filepath,
-        parser = globs[ignore_filepath],
-      }
+      parent = utils.split_path(parent)
     end
 
     for _, item in ipairs(children) do
       if not item.filtered_by or not item.filtered_by.ignore_file then
-        for _, ignorer in ipairs(folder_ignorers) do
-          local parser = ignorer.parser
-          local ignored = parser:check(item.path)
-          if ignored ~= nil then
-            item.filtered_by = item.filtered_by or {}
-            item.filtered_by.ignore_file = ignorer.path
-            item.filtered_by.ignored = ignored
+        for _, ignorer in ipairs(applicable_ignorers) do
+          local path, parser, root = unpack(ignorer)
+          if utils.is_subpath(root, item.path) then
+            local ignored = parser:check(item.path)
+            if ignored ~= nil then
+              item.filtered_by = item.filtered_by or {}
+              item.filtered_by.ignore_file = path
+              item.filtered_by.ignored = ignored
+            end
           end
         end
       end
