005655ba7729710fca525c9fd816a3bfec0f3e16
pynappo <pynappo@proton.me>
2026-04-20 01:15:49 -0700

    cleanup

--- lua/neo-tree/trash/windows.lua
+++ lua/neo-tree/trash/windows.lua
@@ -41,28 +41,39 @@ M.generate_restore_commands = function(paths)
     "-Command",
   }
 
+  --- Assemble a .NET hashset with the items
   local pwsh_cmds = {
-    "$shell = New-Object -ComObject 'Shell.Application';",
-    "$bin = $shell.NameSpace(0xA);",
-    "$items = $bin.Items();",
+    "$paths = New-Object System.Collections.Generic.HashSet[string]();",
   }
 
   for _, path in ipairs(paths) do
-    -- Escape backslashes and single quotes
-    local escaped = path:gsub("\\", "\\\\"):gsub("'", "''")
+    local escaped = path:gsub("'", "''")
+    pwsh_cmds[#pwsh_cmds + 1] = ([[$null = $paths.Add('%s');]]):format(escaped)
+  end
 
-    -- Logic:
-    -- 1. We look at the .Path property of the items in the Recycle Bin
-    -- 2. If it matches the $R... path provided, we restore it
-    local restore_logic = ([[$items | Where-Object { $_.Path -eq '%s' } | ForEach-Object { $_.InvokeVerb('undelete') };]]):format(
-      escaped
-    )
-    pwsh_cmds[#pwsh_cmds + 1] = restore_logic
+  local restore_logic = {
+    "$shell = New-Object -ComObject 'Shell.Application';",
+    "$bin = $shell.NameSpace(0xA);",
+    "$bin.Items() | ForEach-Object {",
+    "  if ($paths.Contains($_.Path)) {",
+    "    try {",
+    "      $_.InvokeVerb('undelete')",
+    "    } catch {",
+    '      [Console]::Error.WriteLine("Failed to restore $($_.Path): $($_.Exception.Message)")',
+    "    }",
+    "  }",
+    "};",
+  }
+
+  for _, v in ipairs(restore_logic) do
+    table.insert(pwsh_cmds, v)
   end
 
   cmd[#cmd + 1] = table.concat(pwsh_cmds, " ")
+
   return {
     cmd,
   }
 end
+
 return M
